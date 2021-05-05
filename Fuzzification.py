bb410781cc2240366d7c37e3b90612170610b26a
paul-gomes <gomesap29@gmail.com>
2021-05-04 19:50:38 -0500

    fuzzification class

---
+++ Fuzzification.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class Fuzzification:
+    def __init__(self, row, mean, sd):
+        self.row = row #each row with multiple features
+        self.mean = mean #means from dynamic clustering for each features
+        self.sd = sd #sds from dynamic clustering for each features
+        
+    def normal_density(x, mean, sd):
+        return (1/(sd*(2*np.pi))**(0.5))* np.exp(-0.5*((x-mean)/sd)**(2))
+    
+    def fuzzify(self):
+        if len(self.row) != len(self.mean):
+            raise Exception('Need means for each feature')
+        if len(self.row != len(self.sd)):
+            raise Exception('Need sd for each feature')
+        fuzzy_output = []    
+        for i in range(self.row):           
+            clusters = len(self.mean[i])
+            output = np.zeros((clusters))
+            densities = []
+            for j in range(clusters):
+                densities.append(self.normal_density(self.row[i], self.mean[i][j], self.sd[i][j]))
+        
+            index = densities.index(max(densities))
+            output[index] = 1
+            fuzzy_output.append(output)
+        return fuzzy_output
