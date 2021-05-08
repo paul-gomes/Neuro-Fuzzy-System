0f6d4242218293c8afe46fb0a6671c113965153d
paul-gomes <gomesap29@gmail.com>
2021-05-08 12:49:55 -0500

    Fuzzification modified

--- Fuzzification.py
+++ Fuzzification.py
@@ -6,21 +6,22 @@ class Fuzzification:
         self.mean = mean #means from dynamic clustering for each features
         self.sd = sd #sds from dynamic clustering for each features
         
-    def normal_density(x, mean, sd):
+    def normal_density(self, x, mean, sd):
         return (1/(sd*(2*np.pi))**(0.5))* np.exp(-0.5*((x-mean)/sd)**(2))
     
     def fuzzify(self):
         if len(self.row) != len(self.mean):
             raise Exception('Need means for each feature')
-        if len(self.row != len(self.sd)):
+        if len(self.row) != len(self.sd):
             raise Exception('Need sd for each feature')
         fuzzy_output = []    
-        for i in range(self.row):           
+        for i in range(len(self.row)):           
             clusters = len(self.mean[i])
             output = np.zeros((clusters))
             densities = []
             for j in range(clusters):
-                densities.append(self.normal_density(self.row[i], self.mean[i][j], self.sd[i][j]))
+                density = self.normal_density(self.row[i], self.mean[i][j], self.sd[i][j])
+                densities.append(density)
         
             index = densities.index(max(densities))
             output[index] = 1
---
+++ main.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data = pd.read_csv("Data/iris.data", header = 0)
+
