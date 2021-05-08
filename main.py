137e0445f70eaec08d30ef0dec009671343ed5a1
paul-gomes <gomesap29@gmail.com>
2021-05-08 13:53:38 -0500

    commented code for better understanding

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ data = bcw_data[bcw_data.columns[1:4]]
 #targets classes 
 t= bcw_data[['diagnosis']].replace(['M','B'],[0,1])
 
+#this is dataframe with cleaned data which has the features and the class(either 0(M) or 1(B))
 df = pd.concat([data, t], axis=1)
 print(df.sample(n=5))
 
-desired_t = [[1,0],[0,1]]
 
-#targets in list format
+desired_t = [[1,0],[0,1]]
+#targets in list format for neural netwrork eg. [0,1] for class being B [1,0] for class being M
 targets = np.array([desired_t[int(x)] for x in df.values[:,3:4]])
 print(targets[:5])
